254b513e8c23a77060438aa08e25c2e4ef9d7444
Nitin Bisht <3498georgian@gmail.com>
2022-05-11 11:13:19 +0530

    Used Reactive prograaming to call an api i.e used WebClient instead of Rest Template

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/controller/MovieCatalogController.java
@@ -3,11 +3,14 @@ package com.example.moviecatalogservice.controller;
 import com.example.moviecatalogservice.model.CatalogItem;
 import com.example.moviecatalogservice.model.Movie;
 import com.example.moviecatalogservice.model.Rating;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,8 +19,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+    private static final Logger Logger = LoggerFactory.getLogger(MovieCatalogController.class);
+
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder builder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -28,8 +36,11 @@ public class MovieCatalogController {
 
         return ratings.stream().map(
                 rating -> {
-                    Movie movie = restTemplate.getForObject(
-                            "http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = builder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+                    Logger.info("Fetch the movie - {}", movie.getName());
                     return new CatalogItem(movie.getName(), "desc", rating.getRating());
                 })
                 .collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/example/ratingsdataservice/controller/RatingsController.java
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/controller/RatingsController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratings")
 public class RatingsController {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(4, movieId);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getRatings(@PathVariable("userId") String userId) {
+        return List.of(
+                new Rating(4, "123"),
+                new Rating(5, "134")
+        );
     }
 }
